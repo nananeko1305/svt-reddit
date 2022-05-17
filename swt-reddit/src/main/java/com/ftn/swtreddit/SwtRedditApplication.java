ae884ae9dd8a20a60d14854442d0c74e8e21bb35
Petar Cocic <cocic.sr16.2020@uns.ac.rs>
2022-05-17 10:58:50 +0200

    Created: Initialized project

---
+++ swt-reddit/src/main/java/com/ftn/swtreddit/SwtRedditApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.swtreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwtRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwtRedditApplication.class, args);
+	}
+
+}
---
+++ swt-reddit/src/test/java/com/ftn/swtreddit/SwtRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.swtreddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwtRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
