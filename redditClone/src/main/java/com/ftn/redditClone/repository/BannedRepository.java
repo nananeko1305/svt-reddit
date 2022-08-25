f8372d88821e68ac0b3519395567c8b4c99ecae9
nananeko1305 <nananeko1305@hotmail.com>
2022-08-25 11:57:09 +0200

    Update

--- redditClone/src/main/java/com/ftn/redditClone/repository/BannedRepository.java
+++ redditClone/src/main/java/com/ftn/redditClone/repository/BannedRepository.java
@@ -3,6 +3,7 @@ package com.ftn.redditClone.repository;
 import com.ftn.redditClone.model.entity.Banned;
 import com.ftn.redditClone.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Repository
 public interface BannedRepository extends JpaRepository<Banned, Integer> {
 
+    @Modifying
     @Query(value = "delete from jpa.banned b where b.id = ?1", nativeQuery = true)
     void delete(int id);
 
